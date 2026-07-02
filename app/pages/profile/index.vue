32f117b92c384f9b592572a965cd4db90664bc74
Kent Joemar Escoto <kentescoto24@gmail.com>
2026-07-02 21:19:12 +0800

    added some content on profile page

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -1,6 +1,6 @@
 <!-- pages/inventory/[uid].vue -->
 <script setup lang="ts">
-import { Home, LogOut } from '@lucide/vue'
+import { ChevronRight, Download, Home, LockKeyhole, LogOut, Tickets, TriangleAlert, Upload } from '@lucide/vue'
 import { format } from 'date-fns'
 
 
@@ -74,17 +74,17 @@ const handleDelete = () => { showDeleteConfirm.value = true }
     <!-- Content -->
     <div class="space-y-8 p-4">
 
-      <section class="flex items-center gap-4">
+      <section class="flex items-center gap-4 p-4 bg-white rounded-2xl">
         <Image :src="photoURL" :alt="user?.displayName ?? 'User'" class="size-20 rounded-full shrink-0"
           referrerpolicy="no-referrer" />
         <div>
           <p class="text-sm text-muted tracking-wide truncate">ID: {{ user?.uid ?? '-' }}</p>
           <p class="text-lg text-black tracking-wide">{{ user?.displayName ?? 'User' }}</p>
-          <p class="text-sm text-black tracking-wide">{{ user?.phone ?? '-' }}</p>
+          <p class="text-sm text-primary tracking-wide">{{ user?.email ?? '-' }}</p>
         </div>
       </section>
 
-      <section class="space-y-2">
+      <section class="space-y-3">
         <h1 class="text-sm text-muted">Account Activity</h1>
         <div class="space-y-0.5 rounded-2xl overflow-hidden">
           <div class="flex items-center justify-between bg-white p-4">
@@ -95,6 +95,71 @@ const handleDelete = () => { showDeleteConfirm.value = true }
             <p class="text-sm">Member Since</p>
             <span class="text-sm font-medium text-black/70">{{ createdAt ?? '—' }}</span>
           </div>
+        </div>
+      </section>
+
+      <section class="space-y-3">
+        <h1 class="text-sm text-muted">Security</h1>
+        <UIButton class="flex items-center justify-between! bg-white p-4! w-full">
+          <div class="flex items-center gap-2">
+            <LockKeyhole class="size-4 text-black/70" />
+            <p class="text-sm text-black/70">Change Password</p>
+          </div>
+          <p class="text-primary text-sm">RESET</p>
+        </UIButton>
+      </section>
+
+      <section class="space-y-3">
+        <h1 class="text-sm text-muted">Help</h1>
+        <UIButton class="flex items-center justify-between! bg-white p-4! w-full">
+          <div class="flex items-center gap-2">
+            <Tickets class="size-4 text-black/70" />
+            <p class="text-sm text-black/70">Submit a Ticket</p>
+          </div>
+          <ChevronRight class="size-4 text-black/70" />
+        </UIButton>
+      </section>
+
+      <section class="space-y-3">
+        <h1 class="text-sm text-muted">Backup</h1>
+        <div class="space-y-0.5 rounded-2xl overflow-hidden">
+          <UIButton class="flex justify-start items-center bg-white p-4! w-full">
+            <Download class="size-4 text-black/70" />
+            <p class="text-sm text-black/70">Export JSON Data</p>
+          </UIButton>
+          <UIButton class="flex justify-start items-center bg-white p-4! w-full">
+            <Upload class="size-4 text-black/70" />
+            <p class="text-sm text-black/70">Import JSON Data</p>
+          </UIButton>
+        </div>
+      </section>
+
+      <section class="space-y-3">
+        <h1 class="text-sm text-muted">About</h1>
+        <div class="space-y-2 bg-white p-4! w-full rounded-2xl">
+          <div class="flex items-center gap-2">
+            <h1 class="text-primary font-bold">E-Lista</h1>
+            <p class="text-xs text-primary w-fit px-3 py-1 rounded-full bg-primary/10">BETA v1.0.0</p>
+          </div>
+          <p class="text-sm text-black/70">A simple inventory and debtor tracker designed to keep your records organized
+            — monitor your products, track stock levels, manage customer balances, and stay updated on what comes in,
+            goes out, and remains unpaid.
+          </p>
+        </div>
+      </section>
+
+      <section class="space-y-3">
+        <h1 class="text-sm text-danger flex items-center gap-2">
+          <TriangleAlert class="size-4 text-danger" />
+          Danger Zone
+        </h1>
+        <div class="space-y-0.5 rounded-2xl overflow-hidden flex gap-2">
+          <UIButton variant="outline" class="bg-white! p-4! w-full border border-danger!">
+            <p class="text-sm text-danger">Erase All Data</p>
+          </UIButton>
+          <UIButton variant="danger" class="w-full p-4!">
+            <p class="text-sm">Delete Account</p>
+          </UIButton>
         </div>
       </section>
     </div>
